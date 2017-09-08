5f9db05dbb3947f20cfe0b20a05663cce9c1a619
Vladimir Zhigalo <Zhigalo.V.Y@gmail.com>
2017-09-08 12:26:51 +0300

    release

--- 02/src/main/java/Sss.java
+++ 02/src/main/java/Sss.java
@@ -1,11 +1,13 @@
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(urlPatterns = "/Sss")
 public class Sss extends HttpServlet {
 
     public Sss() {
@@ -28,23 +30,40 @@ public class Sss extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.service(req, resp);
-        System.out.println("Sss:service:\t" + req.getMethod());
+        System.out.println("Sss:service");
+        System.out.println("Method: " + req.getMethod());
+        System.out.println("Port: " + req.getServerPort());
+
+        PrintWriter pw = resp.getWriter();
+        pw.println("<b><br/>Method:</b> " + req.getMethod());
+        pw.println("<b><br/>Host:</b> " + req.getRemoteHost());
+        pw.println("<b><br/>IP:</b> " + req.getRemoteAddr());
+
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
         resp.setContentType("text/html;charset=utf-8");
-
         PrintWriter pw = resp.getWriter();
-        pw.println("<H1>HALLO</H1>");
-        pw.println("GET");
+
+        String firstName = req.getParameter("fn");
+        String lastName = req.getParameter("ln");
+
+        pw.println(firstName + "\t" + lastName);
+        pw.println("<b><br/>Строка GET-запроса:</b> " + req.getQueryString());
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost");
         resp.setContentType("text/html;charset=utf-8");
 
+        String firstName = req.getParameter("fn");
+        String lastName = req.getParameter("ln");
+
         PrintWriter pw = resp.getWriter();
-        pw.println("POST");
+        pw.println(firstName + "\t" + lastName);
     }
 }
