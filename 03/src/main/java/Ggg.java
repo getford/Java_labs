ef81b8423572cdfe55c22223cdef44573e314581
Vladimir Zhigalo <Zhigalo.V.Y@gmail.com>
2017-09-12 13:02:53 +0300

    just add servlets and html pages

--- 03/src/main/java/Ggg.java
+++ 03/src/main/java/Ggg.java
@@ -6,31 +6,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/Ggg")
+@WebServlet(name = "Ggg", urlPatterns = "/Ggg")
 public class Ggg extends HttpServlet {
+
+    public Ggg() {
+        super();
+        System.out.println("Ggg:constructor");
+    }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+        System.out.println("Ggg:init");
     }
 
     @Override
     public void destroy() {
         super.destroy();
+        System.out.println("Ggg:destroy");
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.service(req, resp);
+        System.out.println("Ggg:service");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+        System.out.println("Ggg:doGet");
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
+        System.out.println("Ggg:doPost");
     }
 }
--- 03/src/main/java/Sss.java
+++ 03/src/main/java/Sss.java
@@ -1,3 +1,4 @@
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -6,31 +7,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet (urlPatterns = "/Sss")
+@WebServlet(name = "Sss", urlPatterns = "/Sss")
 public class Sss extends HttpServlet {
+
+    public Sss() {
+        super();
+        System.out.println("Sss:constructor");
+    }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+        System.out.println("Sss:init");
     }
 
     @Override
     public void destroy() {
         super.destroy();
+        System.out.println("Sss:destroy");
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.service(req, resp);
+        System.out.println("Sss:service");
+
+        RequestDispatcher rd;
+        rd = req.getRequestDispatcher("/Ggg");
+        rd.forward(req, resp);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+        System.out.println("Sss:doGet");
+
+        String fn = req.getParameter("fn");
+        String ln = req.getParameter("ln");
+        String btn = req.getParameter("send");
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
+        System.out.println("Sss:doPost");
     }
 }
