a50fe5a855f724399fe2575464c666e9a9592334
Vladimir Zhigalo <Zhigalo.V.Y@gmail.com>
2017-09-14 17:26:41 +0300

    release task 1

--- 04/src/main/java/jspclass/Time.java
+++ 04/src/main/java/jspclass/Time.java
@@ -1,22 +1,115 @@
 package jspclass;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Time {
-    protected String r = null;
-    private Calendar calendar;
-    protected Date date = new Date();
+    private Calendar calendar = Calendar.getInstance();
+    private Date date = new Date();
+    private int hour = calendar.get(Calendar.HOUR_OF_DAY);
+    private int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+    private int day = calendar.get(Calendar.DAY_OF_MONTH);
+    private int dayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
 
-    public Time(Calendar calendar){
-        this.calendar = calendar;
+    private String rc;
+    private String[][] listDays = new String[0][];
+
+    public Time() {
     }
 
-    public Time(){
-        this.calendar = Calendar.getInstance(); // ?
+    public void PrintHello(int hour) {
+
+        if (hour > 0 && hour <= 5)
+            setRc("Good night");
+        else if (hour > 5 && hour < 12)
+            setRc("Good morning");
+        else if (hour >= 12 && hour <= 17)
+            setRc("Good afternoon");
+        else
+            setRc("Good evening");
     }
 
-    public Integer getHour(){
-        return ((Integer)calendar.get(Calendar.HOUR_OF_DAY));
+    public void ArrayDates(int day, int dayOfWeek, int dayOfMonth, Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(".MM.yyyy");
+        String formatDate = simpleDateFormat.format(date);
+
+        int ni = 7;
+        int nj = 2;
+        String arr[][] = new String[ni][nj];
+
+        for (int i = 0; i < ni; i++) {
+            for (int j = 0; j < nj; j++) {
+                if (j == 0) {
+                    if (day > dayOfMonth)
+                        day = 1;
+                    arr[i][j] = "" + day++ + formatDate;
+                }
+                if (j == 1) {
+                    if (dayOfWeek > 7)
+                        dayOfWeek = 1;
+                    arr[i][j] = "" + dayOfWeek++;
+                }
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        setListDays(arr);
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(int dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getDayOfMonth() {
+        return dayOfMonth;
+    }
+
+    public void setDayOfMonth(int dayOfMonth) {
+        this.dayOfMonth = dayOfMonth;
+    }
+
+    public String getRc() {
+        return rc;
+    }
+
+    public void setRc(String rc) {
+        this.rc = rc;
+    }
+
+    public String[][] getListDays() {
+        return listDays;
+    }
+
+    public void setListDays(String[][] listDays) {
+        this.listDays = listDays;
     }
 }
--- 04/src/main/webapp/index.jsp
+++ 04/src/main/webapp/index.jsp
@@ -1,30 +1,34 @@
 <%@page language="java" contentType="text/html; ISO-8859-1" %>
-<%@ page import="jspclass.*" %>
+<%@ page import="jspclass.Time" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>04 - Index.jsp</title>
 
-    <%!
-        private Time time = new Time();
-        private Integer n = time.getHour();
+    <%--    <%!
+            private Time time = new Time();
+            private Integer n = time.getHour();
 
-        private String Hello(Integer hour) {
-            String rc;
-            if (hour > 0 && hour <= 5)
-                rc = "Good night";
-            else if (hour > 5 && hour <= 12)
-                rc = "Good morning";
-            else if (hour > 12 && hour <= 17)
-                rc = "Good afternoon";
-            else
-                rc = "Good evening";
-            return rc;
-        }
-    %>
+            private String Hello(Integer hour) {
+                String rc;
+                if (hour > 0 && hour <= 5)
+                    rc = "Good night";
+                else if (hour > 5 && hour <= 12)
+                    rc = "Good morning";
+                else if (hour > 12 && hour <= 17)
+                    rc = "Good afternoon";
+                else
+                    rc = "Good evening";
+                return rc;
+            }
+        %>--%>
 </head>
 <body>
-<h4><%=Hello(n)%>
-</h4>
+<% Time time = new Time();%>
+
+<%time.PrintHello(time.getHour());%>
+<%time.ArrayDates(time.getDay(), time.getDayOfWeek(), time.getDayOfMonth(), time.getDate());%>
+<%=time.getRc()%>
 <hr/>
 </body>
 </html>
